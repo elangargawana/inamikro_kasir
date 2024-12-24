430e9ad0f5e867c5df8d01b90039a976761c3ec5
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2024-12-24 14:12:00 +0700

    add auth

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\BaseController;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends BaseController
+{
+    public function login(LoginRequest $request): JsonResponse
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('KlinikApp')->plainTextToken;
+            $success['name'] =  $user->name;
+
+            return $this->sendResponse($success);
+        } else {
+            return $this->sendError('Wrong email or password!');
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            $request->user()->currentAccessToken()->delete();
+            return $this->sendResponse(null);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), 500);
+        }
+    }
+}
