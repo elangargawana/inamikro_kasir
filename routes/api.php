52099add625cdfdb2598d2d2af61effb7569c382
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2025-01-06 09:16:34 +0700

    feat: add route

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Master\KategoriProdukController;
 use App\Http\Controllers\Master\MetodeBayarController;
+use App\Http\Controllers\Master\ProdukController;
 use App\Http\Controllers\Master\SatuanController;
 use App\Http\Controllers\MidtransController;
 use App\Http\Controllers\TransaksiCepatController;
+use App\Http\Controllers\TransaksiPintarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,12 +21,15 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::prefix('/master')->group(function () {
-        Route::resource('/satuan', SatuanController::class);
-        Route::resource('/metode-bayar', MetodeBayarController::class);
+        Route::apiResource('/satuan', SatuanController::class);
+        Route::apiResource('/metode-bayar', MetodeBayarController::class);
+        Route::apiResource('/produk', ProdukController::class);
+        Route::apiResource('/kategori-produk', KategoriProdukController::class);
     });
 
     Route::prefix('/transaksi')->group(function () {
         Route::post('/cepat', [TransaksiCepatController::class, 'store']);
+        Route::post('/pintar', [TransaksiPintarController::class, 'store']);
     });
 });
 
